acda05d22ee61fcd9b54d2657399f42917715264
JeonJaewon <wksk04515@naver.com>
2020-06-12 02:31:33 +0900

    XML Parsing  Completed

--- app/src/main/java/com/example/weather_rabbit/HomeFragment.kt
+++ app/src/main/java/com/example/weather_rabbit/HomeFragment.kt
@@ -1,21 +1,16 @@
 package com.example.weather_rabbit
 
+import android.icu.util.Calendar
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Xml
 import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.fragment_location.*
-import kotlinx.android.synthetic.main.main_toolbar.*
-import java.io.BufferedReader
+import org.xmlpull.v1.XmlPullParser
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
@@ -23,17 +18,14 @@ import java.net.URLEncoder
 class HomeFragment : Fragment(){
     var curX : Double = 0.0
     var curY : Double = 0.0
+    val networkTask = NetworkTask()
+
     lateinit var resText : TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        if(arguments != null){
-            curY = arguments?.getDouble("curY")!!
-            curX = arguments?.getDouble("curX")!!
-            val task = NetworkTask()
-            task.execute(arrayOf(curX,curY)) //매개변수 넣기
-        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -41,20 +33,26 @@ class HomeFragment : Fragment(){
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
+        if(arguments != null){
+            curY = arguments?.getDouble("curY")!!
+            curX = arguments?.getDouble("curX")!!
+            networkTask.execute(arrayOf(curX, curY)) //매개변수 넣기
+        }
     }
-
-    /* You should inflate your layout in onCreateView but shouldn't initialize other views using findViewById in onCreateView.
-    Because sometimes view is not properly initialized.
-    So always use findViewById in onViewCreated(when view is fully created) and it also passes the view as parameter.
-    onViewCreated is a make sure that view is fully created. */
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         resText = resCode
+
         //swipe layout
         swipe_layout.setOnRefreshListener {
             swipe_layout.isRefreshing = true
-            Toast.makeText(activity,"das",Toast.LENGTH_SHORT).show()
+            if(arguments != null){
+                networkTask.cancel(true)
+                curY = arguments?.getDouble("curY")!!
+                curX = arguments?.getDouble("curX")!!
+                networkTask.execute(arrayOf(curX,curY)) //매개변수 넣기
+            }
             swipe_layout.isRefreshing = false
         }
         super.onViewCreated(view, savedInstanceState)
@@ -93,19 +91,30 @@ class HomeFragment : Fragment(){
 //        return super.onOptionsItemSelected(item)
     }
 
-    inner class NetworkTask : AsyncTask<Array<Double>, Double, String>(){
+    inner class NetworkTask : AsyncTask<Array<Double>, Double, Double>(){
         val apiURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"
         val myKey = "%2BxNPCgo3zjIx4PS1ltDnV0bHyyELDt5Ed6L4GZvyC8ALqZBDlWy6bEda40xrmxyUIjjDBFYnA6t372jmh2SCEQ%3D%3D"
         lateinit var urlBuilder: StringBuilder
 
-        override fun doInBackground(vararg p0: Array<Double>?): String? {
+        val STEP_CATEGORY = 0
+        val STEP_FCST_VALUE = 1
+
+        override fun doInBackground(vararg p0: Array<Double>): Double? {
             // api url 생성
+            val calendar =Calendar.getInstance()
+            val baseTime = getLastBaseTime(calendar)
+            var year = baseTime?.get(Calendar.YEAR).toString()
+            var month = zeroAdder(baseTime!!.get(Calendar.MONTH)+1)
+            var day = zeroAdder(baseTime!!.get(Calendar.DAY_OF_MONTH))
+            var hour = zeroAdder(baseTime!!.get(Calendar.HOUR_OF_DAY))
+            var minute = zeroAdder(baseTime!!.get(Calendar.MINUTE))
+
             urlBuilder = StringBuilder(apiURL)
             urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + myKey)
             urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + 10)
             urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + 1)
-            urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + "20200605")
-            urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + "1400")
+            urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + year + month + day)
+            urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + hour + minute)
             urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + p0[0]?.get(0)?.toInt())
             urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + p0[0]?.get(1)?.toInt())
 
@@ -114,19 +123,77 @@ class HomeFragment : Fragment(){
             connection.requestMethod = "GET"
             connection.setRequestProperty("Content-Type", "application/xml")
 
-            val br = BufferedReader(InputStreamReader(connection.inputStream))
-            var line : String = ""
-            line = br.readLine()
-            return connection.responseCode.toString()
-//        while(!line.equals("")){
-//        }
-//        Toast.makeText(activity, connection.responseCode, Toast.LENGTH_SHORT).show()
-//        Toast.makeText(activity, curX.toString(), Toast.LENGTH_SHORT).show()
+            val parser = Xml.newPullParser()
+            var step = -1
+            var eventType = parser.eventType
+            var t3hValue : Double = 0.0
+            var isT3H = false
+            parser.setInput(InputStreamReader(connection.inputStream))
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    when(parser.name){
+                        "category" -> {
+                            if(parser.nextText() == "T3H"){
+                                step = STEP_CATEGORY
+                                isT3H = true
+                            }
+                        }
+                        "fcstValue" -> {
+                            if(isT3H){
+                                t3hValue = parser.nextText().toDouble()
+                                isT3H = false
+                            }
+                        }
+                    }
+                }
+                eventType = parser.next()
+            }
+            return t3hValue
+//            return connection.responseCode.toString()
         }
 
-        override fun onPostExecute(result: String?) {
+        override fun onPostExecute(result: Double?) {
             super.onPostExecute(result)
-            resText.text = result
+            resText.text = result.toString()
+        }
+
+        // 일의자리 수일 경우 앞에 0 붙이기
+        private fun zeroAdder(num : Int) : String {
+            if(num <= 9){
+                val str = "0" + (num).toString()
+                return str
+            } else {
+                return (num).toString()
+            }
+        }
+
+        private fun getLastBaseTime(calBase: Calendar): Calendar? {
+            val baseArr = arrayOf(2, 5, 8, 11, 14, 17, 20, 23)
+            val hour = calBase[Calendar.HOUR_OF_DAY]
+            val minute = calBase[Calendar.MINUTE]
+            if (hour < 2) {
+                calBase.add(Calendar.DATE, -1)
+                calBase[Calendar.HOUR_OF_DAY] = 23
+            } else {
+                calBase[Calendar.HOUR_OF_DAY] = hour - (hour + 1) % 3
+                for(i in 0..7){
+                    if(hour == baseArr[i]) {
+                        // 10분 후에야 정보를 받아올 수 있음..
+                        if(minute <= 10){
+                            if(i - 1 == -1){
+                                calBase.add(Calendar.DATE, -1)
+                                calBase[Calendar.HOUR_OF_DAY] = baseArr[7]
+                            }
+                            else
+                                calBase[Calendar.HOUR_OF_DAY] = baseArr[i-1]
+                            break
+                        }
+                    }
+                }
+            }
+            calBase[Calendar.MINUTE] = 0
+            return calBase
         }
     }
+
 }
