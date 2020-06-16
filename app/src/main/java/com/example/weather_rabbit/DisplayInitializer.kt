1dbd4a42eaaaa44d73eae177c4a938720f155a35
JeonJaewon <wksk04515@naver.com>
2020-06-16 17:40:29 +0900

    card fragment add

--- app/src/main/java/com/example/weather_rabbit/DisplayInitializer.kt
+++ app/src/main/java/com/example/weather_rabbit/DisplayInitializer.kt
@@ -1,7 +1,17 @@
 package com.example.weather_rabbit
 
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.util.*
 
-class DisplayInitializer(val frag : HomeFragment){
+class DisplayInitializer(val frag: HomeFragment) {
+    fun init() {
+        val calendar = Calendar.getInstance()
+        val dateStr = calendar.get(Calendar.YEAR)
+            .toString() + " " + calendar.get(Calendar.MONTH) + " " + calendar.get(Calendar.DAY_OF_MONTH)
 
+        val dateText = frag.activity?.findViewById<TextView>(R.id.date_text)
+        dateText?.text = dateStr
+    }
 }
+
